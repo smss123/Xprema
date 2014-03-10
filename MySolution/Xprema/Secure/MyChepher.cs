d0914ccd45a27228f93a001116088e896ff74aca
smss123 <smsssamer@live.com>
2014-03-11 02:09:14 +0400

    Secure Name Space

---
+++ MySolution/Xprema/Secure/MyChepher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xprema.Secure
+{
+    class MyChepher
+    {
+
+        // AES Chepher
+        //Des Chepher
+
+    }
+}
