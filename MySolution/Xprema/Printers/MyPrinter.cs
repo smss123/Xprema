68ec0c0ddc96200b6ddb5d1a95efc4920a61c19c
smss123 <smsssamer@live.com>
2014-03-11 01:18:39 +0400

    Adding Sum Function

--- MySolution/Xprema/Printers/MyPrinter.cs
+++ MySolution/Xprema/Printers/MyPrinter.cs
@@ -8,5 +8,15 @@ namespace Xprema.Printers
    public  class MyPrinter
     {
        public MyPrinter() { }
+
+       /// <summary>
+       /// to Sum 2 int 
+       /// </summary>
+       /// <param name="A"></param>
+       /// <param name="B"></param>
+       /// <returns></returns>
+       public int Sum(int A, int B) {
+           return (A + B);
+       }
     }
 }
