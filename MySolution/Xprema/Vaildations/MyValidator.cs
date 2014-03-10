3adad1545ef06a640cc0ccae930a5afdefa04b34
smss123 <smsssamer@live.com>
2014-03-11 01:26:16 +0400

    Add Validtor

---
+++ MySolution/Xprema/Vaildations/MyValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xprema.Vaildations
+{
+   public  class MyValidator
+    {
+       public MyValidator() { }
+    }
+}
