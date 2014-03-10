c8dcd59b76594b5fb491206b1dd52af9867840db
ainma007 <b-sword@outlook.com>
2014-03-10 23:34:58 +0200

    add new Get DefaultPrinter

--- MySolution/Xprema/Printers/MyPrinter.cs
+++ MySolution/Xprema/Printers/MyPrinter.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing.Printing;
 
 namespace Xprema.Printers
 {
    public  class MyPrinter
     {
        public MyPrinter() { }
-    
-       
+
+      public string GetDefaultPrinter()
+       {
+           PrinterSettings settings = new PrinterSettings();
+           foreach (string printer in PrinterSettings.InstalledPrinters)
+           {
+               settings.PrinterName = printer;
+               if (settings.IsDefaultPrinter)
+                   return printer;
+           }
+           return string.Empty;
+       }
     
       
     }
